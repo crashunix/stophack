ac83c19fc1ed932759f0870397ab4433a0fc21eb
crashunix <jgomesmacario@gmail.com>
2020-06-19 06:04:05 -0300

    frontend-mobile

---
+++ frontend-mobile/src/screens/Home/index.js
@@ -0,0 +1,97 @@
+import React, { Component, useEffect, useState } from 'react';
+import { Text, View, CheckBox } from 'react-native';
+
+import axios from 'axios';
+
+import { MaterialCommunityIcons, AntDesign, Ionicons } from '@expo/vector-icons';
+
+import { 
+    Wrapper, 
+    Header, 
+    Container, 
+    Logo, 
+    Nav, 
+    NavItem, 
+    NoConnection, 
+    Group, 
+    Title, 
+    ScrollGroups, 
+    Form, 
+    TextInput, 
+    Result,
+    Label,
+    Word,
+    ScrollResults,
+    Button
+} from './styles';
+
+import logo from '../../images/Netflix-logo2.png';
+import { Divider } from 'react-native-elements';
+
+export default function Home() {
+
+    const [groups, setGroups] = useState([]);
+    const [groupsSelected, setGroupsSelected] = useState([]);
+    const [results, setResults] = useState([]);
+    const [text, setText] = useState([]);
+
+    async function getGroups() {
+        const response = await fetch('https://stophack.herokuapp.com/api/wordgroups');
+        const data = await response.json();
+        setGroups(data);
+    }
+
+    useEffect(() => {
+        getGroups();
+    }, []);
+
+    function selectGroup(id) {
+        const alreadySelected = groupsSelected.findIndex(item => item === id);
+
+        console.log("Selected");
+
+        if (alreadySelected >= 0) {
+            const filteredItems = groupsSelected.filter(item => item !== id);
+            setGroupsSelected(filteredItems);
+        } else {
+            setGroupsSelected([...groupsSelected, id]);
+        }
+    }
+
+    function hack(letter) {
+        axios.post(`https://stophack.herokuapp.com/api/hack`, {
+            wordGroups: groupsSelected,
+            letter: letter
+        }).then(res => {
+            setResults(res.data);
+            console.log(results);
+        })
+    }
+
+    return (
+        <Wrapper>
+            <Container>
+                <Header>
+                    <Title>Montar jogo</Title>
+                    <Ionicons name="md-car" size={32} color="#999" />
+                </Header>
+                <Divider></Divider>
+                <ScrollGroups>
+                    {groups.map(group => (
+                        <Group check={groupsSelected.includes(group._id)} onPress={() => selectGroup(group._id)} key={group._id}><Text>{group.label}</Text></Group>
+                    ))}
+                </ScrollGroups>
+                <Divider></Divider>
+                <Form>
+                    <TextInput onChangeText={text => setText(text)}></TextInput>
+                    <Button onPress={() => hack(text)} title="Buscar"></Button>
+                </Form>
+                <ScrollResults>
+                    {results.map(result => (
+                        <Result key={result._id}><Label>{result.wordGroup.label}</Label><Word>{result.label}</Word></Result>
+                    ))}
+                </ScrollResults>
+            </Container>
+        </Wrapper>
+    );
+}
---
+++ frontend-mobile/src/screens/Home/styles.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components/native';
+
+export const Wrapper = styled.View`
+background: #fff;
+flex: 1;
+`;
+
+export const Header = styled.View`
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    z-index: 999;
+    padding: 10px 15px;
+`;
+
+export const Title = styled.Text`
+    font-size: 32px
+`;
+
+export const Container = styled.ScrollView``;
+
+export const Logo = styled.Image`
+    width: 46px;
+    height: 46px;
+    margin-left: -20px;
+`;
+
+export const Nav = styled.View`
+    color: #fff;
+    display: flex;
+    flex-direction: row;
+`;
+
+export const NavItem = styled.Text`
+    color: #fff;
+    font-size: 16px;
+    margin: 0 10px;
+`;
+
+export const NoConnection = styled.Text`
+    text-align: center;
+    height: 100%;
+    font-size: 16px;
+    color: #fff;
+`;
+
+export const Group = styled.TouchableOpacity`
+    padding: 15px;
+    ${props => props.check ? 'background: #e1e1e1' : 'background: white'};
+`;
+
+export const ScrollGroups = styled.ScrollView`
+    max-height: 400px;
+    min-height: 400px;
+`;
+
+export const Form = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px;
+`;
+
+export const TextInput = styled.TextInput`
+    border: 1px solid #ccc;
+    margin: 0 10px 0 0;
+`;
+
+export const Button = styled.Button`
+    flex: 1;
+`;
+
+export const ScrollResults = styled.ScrollView`
+    max-height: 215px;
+    min-height: 215px;
+`;
+
+export const Result = styled.View`
+    flex-direction: column;
+    padding: 15px;
+`;
+
+export const Label = styled.Text`
+    font-size: 14px;
+`;
+export const Word = styled.Text`
+    font-size: 18px;
+`;
